e4ac103a5bee1d125c23526ae0b26a7e54354815
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2022-04-09 11:38:40 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -2558,7 +2558,7 @@ break
             case 'tiktokmp3': case 'tiktokaudio': {
                 if (!text) throw 'Masukkan Query Link!'
                 m.reply(mess.wait)
-                let anu = await fetchJson(api(zenz', '/downloader/tiktok', { url: text }, 'apikey'))
+                let anu = await fetchJson(api('zenz', '/downloader/tiktok', { url: text }, 'apikey'))
                 let buttons = [
                     {buttonId: `tiktoknowm ${text}`, buttonText: {displayText: '► No Watermark'}, type: 1},
                     {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1}
